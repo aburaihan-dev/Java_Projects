9d24ea488fd38ca2d20da435af7297bcaa31502d
ARSrabon <m.arsrabon@gmail.com>
2016-10-07 23:11:31 +0600

    prob 1018 solved

--- URI Online Judge Beginner Problem Solve/src/beginners_problem_practice/Banknotes_1018.java
+++ URI Online Judge Beginner Problem Solve/src/beginners_problem_practice/Banknotes_1018.java
@@ -13,37 +13,50 @@ public class Banknotes_1018 {
 
         long amount = scan.nextInt();
         System.out.println(amount);
-        if(amount > 100){
-            System.out.println(amount/100);
+        if (amount >= 100) {
+            System.out.println((amount / 100) + " nota(s) de R$ 100,00");
             amount %= 100;
+        } else {
+            System.out.println("0 nota(s) de R$ 100,00");
         }
-        if(amount > 50){
-            System.out.println(amount/50);
+
+        if (amount >= 50) {
+            System.out.println(amount / 50 + " nota(s) de R$ 50,00");
             amount %= 50;
+        } else {
+            System.out.println("0 nota(s) de R$ 50,00");
         }
-        if(amount > 20){
-            System.out.println(amount/20);
+
+        if (amount >= 20) {
+            System.out.println(amount / 20 + " nota(s) de R$ 20,00");
             amount %= 20;
+        }else {
+            System.out.println("0 nota(s) de R$ 20,00");
         }
-        if(amount > 100){
-            System.out.println(amount/100);
-            amount %= 100;
-        }
-        if(amount > 10){
-            System.out.println(amount/10);
+
+        if (amount >= 10) {
+            System.out.println(amount / 10 + " nota(s) de R$ 10,00");
             amount %= 10;
+        } else {
+            System.out.println("0 nota(s) de R$ 10,00");
         }
-        if(amount > 5){
-            System.out.println(amount/5);
+        if (amount >= 5) {
+            System.out.println(amount / 5 + " nota(s) de R$ 5,00");
             amount %= 5;
+        }else{
+            System.out.println("0 nota(s) de R$ 5,00");
         }
-        if(amount > 2){
-            System.out.println(amount/2);
+        if (amount >= 2) {
+            System.out.println(amount / 2 + " nota(s) de R$ 2,00");
             amount %= 2;
+        }else {
+            System.out.println("0 nota(s) de R$ 2,00");
         }
-        if(amount > 1){
-            System.out.println(amount/1);
+        if (amount >= 1) {
+            System.out.println(amount / 1 + " nota(s) de R$ 1,00");
 //            amount %= 1;
+        }else {
+            System.out.println("0 nota(s) de R$ 1,00");
         }
 
     }
